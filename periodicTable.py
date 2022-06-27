b66218dcc0b012db0a40613243143298e92dd5a9
Fazalullah Jafar <fazalullahjafar@gmail.com>
2022-06-27 23:19:27 +0400

    improved periodictable.py

--- periodicTable.py
+++ periodicTable.py
@@ -1,81 +1,159 @@
 elements = [
+    
     {"Element Name": "Hydrogen", "Atomic Number":"1","Atomic Mass":"1.0078","Symbol":"H"},
+    
     {"Element Name": "Helium", "Atomic Number":"2","Atomic Mass":"4.0026","Symbol":"He"},
+    
     {"Element Name": "Lithium", "Atomic Number":"3","Atomic Mass":"6.9410","Symbol":"Li"},
+    
     {"Element Name": "Beryllium", "Atomic Number":"4","Atomic Mass":"9.0122","Symbol":"Be"},
+    
     {"Element Name": "Boron", "Atomic Number":"5","Atomic Mass":"10.811","Symbol":"B"},
+    
     {"Element Name": "Carbon", "Atomic Number":"6","Atomic Mass":"12.011","Symbol":"C"},
+    
     {"Element Name": "Nitrogen", "Atomic Number":"7","Atomic Mass":"14.007","Symbol":"N"},
+    
     {"Element Name": "Oxygen", "Atomic Number":"8","Atomic Mass":"15.999","Symbol":"O"},
+    
     {"Element Name": "Fluorine", "Atomic Number":"9","Atomic Mass":"18.998","Symbol":"F"},
+    
     {"Element Name": "Neon", "Atomic Number":"10","Atomic Mass":"20.180","Symbol":"Ne"},
+    
     {"Element Name": "Sodium", "Atomic Number":"11","Atomic Mass":"22.990","Symbol":"Na"},
+    
     {"Element Name": "Magnesium", "Atomic Number":"12","Atomic Mass":"24.305","Symbol":"Mg"},
+    
     {"Element Name": "Aluminium", "Atomic Number":"13","Atomic Mass":"26.982","Symbol":"Al"},
+    
     {"Element Name": "Silicon", "Atomic Number":"14","Atomic Mass":"28.086","Symbol":"Si"},
+    
     {"Element Name": "Phosphorus", "Atomic Number":"15","Atomic Mass":"30.974","Symbol":"P"},
+    
     {"Element Name": "Sulfur", "Atomic Number":"16","Atomic Mass":"32.066","Symbol":"S"},
+    
     {"Element Name": "Chlorine", "Atomic Number":"17","Atomic Mass":"35.453","Symbol":"Cl"},
+    
     {"Element Name": "Argon", "Atomic Number":"18","Atomic Mass":"39.948","Symbol":"Ar"},
+    
     {"Element Name": "Potassium", "Atomic Number":"19","Atomic Mass":"39.098","Symbol":"K"},
+    
     {"Element Name": "Calcium", "Atomic Number":"20","Atomic Mass":"40.078","Symbol":"Ca"},
+    
     {"Element Name": "Scandium", "Atomic Number":"21","Atomic Mass":"44.956","Symbol":"Sc"},
+    
     {"Element Name": "Titanium", "Atomic Number":"22","Atomic Mass":"47.867","Symbol":"Ti"},
+    
     {"Element Name": "Vanadium", "Atomic Number":"23","Atomic Mass":"50.942","Symbol":"V"},
+    
     {"Element Name": "Chromium", "Atomic Number":"24","Atomic Mass":"51.996","Symbol":"Cr"},
+    
     {"Element Name": "Manganese", "Atomic Number":"25","Atomic Mass":"54.938","Symbol":"Mn"},
+    
     {"Element Name": "Iron", "Atomic Number":"26","Atomic Mass":"55.845","Symbol":"Fe"},
+    
     {"Element Name": "Cobalt", "Atomic Number":"27","Atomic Mass":"58.933","Symbol":"Co"},
+    
     {"Element Name": "Nickel", "Atomic Number":"28","Atomic Mass":"58.693","Symbol":"Ni"},
+    
     {"Element Name": "Copper", "Atomic Number":"29","Atomic Mass":"63.546","Symbol":"Cu"},
+    
     {"Element Name": "Zinc", "Atomic Number":"30","Atomic Mass":"65.38","Symbol":"Zn"},
+    
     {"Element Name": "Gallium", "Atomic Number":"31","Atomic Mass":"69.723","Symbol":"Ga"},
+    
     {"Element Name": "Germanium", "Atomic Number":"32","Atomic Mass":"72.631","Symbol":"Ge"},
+    
     {"Element Name": "Arsenic", "Atomic Number":"33","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Selenium", "Atomic Number":"34","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Bromine", "Atomic Number":"35","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Krypton", "Atomic Number":"36","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Rubidium", "Atomic Number":"37","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Strontium", "Atomic Number":"38","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Yttrium", "Atomic Number":"39","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Zirconium", "Atomic Number":"40","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Niobium", "Atomic Number":"41","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Molybdenum", "Atomic Number":"42","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Technetium", "Atomic Number":"43","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Ruthenium", "Atomic Number":"44","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Rhodium", "Atomic Number":"45","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Palladium", "Atomic Number":"46","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Silver", "Atomic Number":"47","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Cadmium", "Atomic Number":"48","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Indium", "Atomic Number":"49","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Tin", "Atomic Number":"50","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Antimony", "Atomic Number":"51","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Tellurium", "Atomic Number":"52","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Iodine", "Atomic Number":"53","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Xenon", "Atomic Number":"54","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Cesium", "Atomic Number":"55","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Barium", "Atomic Number":"56","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Lanthanum", "Atomic Number":"57","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Cerium", "Atomic Number":"58","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Praseodymium", "Atomic Number":"59","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Neodymium", "Atomic Number":"60","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Promethium", "Atomic Number":"61","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Samarium", "Atomic Number":"62","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Europium", "Atomic Number":"63","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Gadolinium", "Atomic Number":"64","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Terbium", "Atomic Number":"65","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Dysprosium", "Atomic Number":"66","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Holmium", "Atomic Number":"67","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Erbium", "Atomic Number":"68","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Thulium", "Atomic Number":"69","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Ytterbium", "Atomic Number":"70","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Lutenium", "Atomic Number":"71","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Hafnium", "Atomic Number":"72","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Tantalum", "Atomic Number":"73","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Tungsten", "Atomic Number":"74","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Rhenium", "Atomic Number":"75","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Osmium", "Atomic Number":"76","Atomic Mass":"74.922","Symbol":"As"},
-    {"Element Name": "Iridium", "Atomic Number":"77","Atomic Mass":"74.922","Symbol":"As"},
+    
+    {"Element Name": "Selenium", "Atomic Number":"34","Atomic Mass":"78.972","Symbol":"Se"},
+    
+    {"Element Name": "Bromine", "Atomic Number":"35","Atomic Mass":"79.904","Symbol":"Br"},
+    
+    {"Element Name": "Krypton", "Atomic Number":"36","Atomic Mass":"84.798","Symbol":"Kr"},
+    
+    {"Element Name": "Rubidium", "Atomic Number":"37","Atomic Mass":"85.468","Symbol":"Rb"},
+    
+    {"Element Name": "Strontium", "Atomic Number":"38","Atomic Mass":"87.62","Symbol":"Sr"},
+    
+    {"Element Name": "Yttrium", "Atomic Number":"39","Atomic Mass":"88.906","Symbol":"Y"},
+    
+    {"Element Name": "Zirconium", "Atomic Number":"40","Atomic Mass":"91.224","Symbol":"Zr"},
+    
+    {"Element Name": "Niobium", "Atomic Number":"41","Atomic Mass":"92.906","Symbol":"Nb"},
+    
+    {"Element Name": "Molybdenum", "Atomic Number":"42","Atomic Mass":"95.95","Symbol":"Mo"},
+    
+    {"Element Name": "Technetium", "Atomic Number":"43","Atomic Mass":"98.907","Symbol":"Tc"},
+    
+    {"Element Name": "Ruthenium", "Atomic Number":"44","Atomic Mass":"101.07","Symbol":"Ru"},
+    
+    {"Element Name": "Rhodium", "Atomic Number":"45","Atomic Mass":"102.906","Symbol":"Rh"},
+    
+    {"Element Name": "Palladium", "Atomic Number":"46","Atomic Mass":"106.42","Symbol":"Pd"},
+    
+    {"Element Name": "Silver", "Atomic Number":"47","Atomic Mass":"107.868","Symbol":"Ag"},
+    
+    {"Element Name": "Cadmium", "Atomic Number":"48","Atomic Mass":"112.411","Symbol":"Cd"},
+    
+    {"Element Name": "Indium", "Atomic Number":"49","Atomic Mass":"114.818","Symbol":"In"},
+    
+    {"Element Name": "Tin", "Atomic Number":"50","Atomic Mass":"118.711","Symbol":"Sn"},
+    
+    {"Element Name": "Antimony", "Atomic Number":"51","Atomic Mass":"121.760","Symbol":"Sb"},
+    
+    {"Element Name": "Tellurium", "Atomic Number":"52","Atomic Mass":"127.6","Symbol":"Te"},
+    
+    {"Element Name": "Iodine", "Atomic Number":"53","Atomic Mass":"126.904","Symbol":"I"},
+    
+    {"Element Name": "Xenon", "Atomic Number":"54","Atomic Mass":"131.294","Symbol":"Xe"},
+    
+    {"Element Name": "Cesium", "Atomic Number":"55","Atomic Mass":"132.905","Symbol":"Cs"},
+    
+    {"Element Name": "Barium", "Atomic Number":"56","Atomic Mass":"137.328","Symbol":"Ba"},
+    
+    {"Element Name": "Lanthanum", "Atomic Number":"57","Atomic Mass":"138.905","Symbol":"La"},
+    
+    {"Element Name": "Cerium", "Atomic Number":"58","Atomic Mass":"140.116","Symbol":"Ce"},
+    
+    {"Element Name": "Praseodymium", "Atomic Number":"59","Atomic Mass":"140.908","Symbol":"Pr"},
+    
+    {"Element Name": "Neodymium", "Atomic Number":"60","Atomic Mass":"144.242","Symbol":"Nd"},
+    
+    {"Element Name": "Promethium", "Atomic Number":"61","Atomic Mass":"144.913","Symbol":"Pm"},
+    
+    {"Element Name": "Samarium", "Atomic Number":"62","Atomic Mass":"150.36","Symbol":"Sm"},
+    
+    {"Element Name": "Europium", "Atomic Number":"63","Atomic Mass":"151.964","Symbol":"Eu"},
+    
+    {"Element Name": "Gadolinium", "Atomic Number":"64","Atomic Mass":"157.25","Symbol":"Gd"},
+    
+    {"Element Name": "Terbium", "Atomic Number":"65","Atomic Mass":"158.925","Symbol":"Tb"},
+    
+    {"Element Name": "Dysprosium", "Atomic Number":"66","Atomic Mass":"162.500","Symbol":"Dy"},
+    
+    {"Element Name": "Holmium", "Atomic Number":"67","Atomic Mass":"164.930","Symbol":"Ho"},
+    
+    {"Element Name": "Erbium", "Atomic Number":"68","Atomic Mass":"167.259","Symbol":"Er"},
+    
+    {"Element Name": "Thulium", "Atomic Number":"69","Atomic Mass":"168.934","Symbol":"Tm"},
+    
+    {"Element Name": "Ytterbium", "Atomic Number":"70","Atomic Mass":"173.055","Symbol":"Yb"},
+    
+    {"Element Name": "Lutetium", "Atomic Number":"71","Atomic Mass":"174.967","Symbol":"Lu"},
+    
+    {"Element Name": "Hafnium", "Atomic Number":"72","Atomic Mass":"178.49","Symbol":"Hf"},
+    
+    {"Element Name": "Tantalum", "Atomic Number":"73","Atomic Mass":"180.948","Symbol":"Ta"},
+    
+    {"Element Name": "Tungsten", "Atomic Number":"74","Atomic Mass":"183.84","Symbol":"W"},
+    
+    {"Element Name": "Rhenium", "Atomic Number":"75","Atomic Mass":"186.207","Symbol":"Re"},
+    
+    {"Element Name": "Osmium", "Atomic Number":"76","Atomic Mass":"190.23","Symbol":"Os"},
+    
+    {"Element Name": "Iridium", "Atomic Number":"77","Atomic Mass":"192.217","Symbol":"Ir"},
+
 
 ]
 
